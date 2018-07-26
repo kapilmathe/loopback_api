961c029cc750c73d3a28f369639ef9692e5a44bd
kapil mathe <kapilmathe2000@gmail.com>
2018-07-27 02:53:46 +0530

    loopback scaffolded rest_api

---
+++ app1/common/models/cat.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(Cat) {
+
+
+    Cat.observe('before save', function(context, next) {
+        if(context.instance) context.instance.updated = new Date();
+        next();
+    });
+
+    Cat.afterRemote('findById', function(context, cat, next){
+        cat.description = cat.name + ' is ' + cat.age +
+        ' years old and is a ' + cat.breed;
+        next();
+    });
+    
+    Cat.adoptable = function(id, cb) {
+        Cat.findById(id, function(err, cat) {
+            if(err) return cb("Error", null);
+            if(!cat) return cb("cat not found", null);
+            let canAdopt = false;
+            if(cat.breed != 'tiger' || (cat.age >= 10))
+            canAdopt = true;
+            cb(null, canAdopt);
+        });
+    }
+
+    Cat.remoteMethod('adoptable', {
+        accepts: {arg: 'id', type: 'any'},
+        returns: {arg: 'adoptable', type: 'boolean'},
+    });
+};
